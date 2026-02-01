99281da170fd6b275421456ebc71bd2520ef917b
Nageswar <nagesh23479@gmail.com>
2026-02-01 13:58:46 +0530

    mongo Update

--- frontend.sh
+++ frontend.sh
@@ -18,9 +18,10 @@ fi
 
 validate(){
     if [ $1 -ne 0 ]; then
-    echo -e "$2 .... $G Success $N" | tee -a $LOGS_FILE
+    echo -e "$2 .... $G Failed $N" | tee -a $LOGS_FILE
+    exit 1
     else 
-    echo -e "$2 .... $R Failed $N" | tee -a $LOGS_FILE
+    echo -e "$2 .... $R Suceess $N" | tee -a $LOGS_FILE
     fi
 }
 
